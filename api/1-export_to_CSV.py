8e59fe8ffd4452e95123a577e42fdf3bf6e103ab
MoeAlAnnan <5653@holbertonstudents.com>
2023-12-19 10:34:25 +0000

    missing plus

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,6 +8,8 @@ export data in the CSV format.
 import csv
 import requests
 import sys
+
+
 if __name__ == '__main__':
     """ EXTRACT USER DATA """
     emp_id = sys.argv[1]
@@ -17,7 +19,8 @@ if __name__ == '__main__':
 
     """ EXTRACT TASK """
     extract_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(emp_id) '/todos')
+        'https://jsonplaceholder.typicode.com/users/{}'.format(emp_id) +
+        '/todos')
     with open("{}.csv".format(sys.argv[1]), "w") as file_c:
         writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
         for task in extract_employee.json():
